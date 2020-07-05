8a9185309f45ff8ab32b8c84f7b5bb4457d5a769
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2020-07-04 19:28:20 -0600

    Se puso controles para verificar por qué se cae

--- WinFindLexeme/Form1.Designer.cs
+++ WinFindLexeme/Form1.Designer.cs
@@ -31,26 +31,31 @@
             this.txtInput = new System.Windows.Forms.TextBox();
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.butDo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTableName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtInput
             // 
             this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtInput.Location = new System.Drawing.Point(-2, 32);
+            this.txtInput.Location = new System.Drawing.Point(-2, 35);
             this.txtInput.Multiline = true;
             this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.txtInput.Size = new System.Drawing.Size(897, 207);
             this.txtInput.TabIndex = 0;
             // 
             // txtOutput
             // 
-            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtOutput.Location = new System.Drawing.Point(-2, 402);
+            this.txtOutput.Location = new System.Drawing.Point(-2, 374);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
-            this.txtOutput.Size = new System.Drawing.Size(897, 207);
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(897, 235);
             this.txtOutput.TabIndex = 1;
             // 
             // butDo
@@ -63,11 +68,29 @@
             this.butDo.UseVisualStyleBackColor = true;
             this.butDo.Click += new System.EventHandler(this.butDo_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Table Name:";
+            // 
+            // txtTableName
+            // 
+            this.txtTableName.Location = new System.Drawing.Point(128, 2);
+            this.txtTableName.Name = "txtTableName";
+            this.txtTableName.Size = new System.Drawing.Size(502, 26);
+            this.txtTableName.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(890, 608);
+            this.Controls.Add(this.txtTableName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.butDo);
             this.Controls.Add(this.txtOutput);
             this.Controls.Add(this.txtInput);
@@ -86,6 +109,8 @@
         private System.Windows.Forms.TextBox txtInput;
         private System.Windows.Forms.TextBox txtOutput;
         private System.Windows.Forms.Button butDo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTableName;
     }
 }
 
--- WinFindLexeme/Form1.cs
+++ WinFindLexeme/Form1.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,32 +18,93 @@ namespace WinFindLexeme
         public Form1()
         {
             InitializeComponent();
+            InitMyComponents();
         }
 
-        
+        private void InitMyComponents()
+        {
+            string pathFileSpec = ConfigurationManager.AppSettings["pathSpec"];
+            txtInput.Text = File.ReadAllText(pathFileSpec);
+
+        }
+
+        List<string> FindLexemes(string pTexto)
+        {
+            List<string> res = null;
+            string scrap = string.Empty; ;
+            int vcb = 0;
+            int inic = 0;
+            int fin = 0;
+            int tolerancia = 10000;
+            int cont = 0;
+            try
+            {
+                res = new List<string>();
+               // while (vcb < pTexto.Length && cont <= tolerancia)
+                 while (vcb < pTexto.Length )
+                {
+                    Console.WriteLine("vcb = " + vcb.ToString());
+                    Console.WriteLine("cont = " + cont.ToString());
+                    if (vcb == 15986)
+                    {
+                        Console.WriteLine("cont = " + cont.ToString());
+                    }
+                    ++cont;
+                    vcb = pTexto.IndexOf("<%", inic);
+                    if (vcb >= 0)
+                    {
+                        // busca el cierre
+                        fin = pTexto.IndexOf(">", vcb);
+                        if (fin >= 0)
+                        {
+                            scrap = pTexto.Substring(vcb, fin - vcb + 1);
+                            if (!res.Contains(scrap))
+                            {
+                                res.Add(scrap);
+                            }
+                            inic = vcb = fin + 1;
+                        }
+                        else
+                        {
+                            vcb = pTexto.Length;
+                        }
+                    }
+                    else
+                    {
+                        vcb = pTexto.Length;
+                    }
+                }
+                return res;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
 
         private void butDo_Click(object sender, EventArgs e)
         {
-            List<string> lst = new List<string>();
-            string scrap;
 
-            lst.Add ("<%lex1>");
-            lst.Add ("<%lex2>");
+            List<string> lst = null;
 
-            string hil1 = "<%lex1>  donde el mundo viaja <%lex2>";
-            txtInput.Text = hil1;
-            txtOutput.Text = hil1;
-
-            // Recolectar Lexemas
-
-            foreach (var item in lst)
+            try
             {
+                lst = FindLexemes(txtInput.Text);
 
-                scrap =  txtOutput.Text.Replace(item, "xxxxxx");
-                txtOutput.Text = scrap;
+                foreach (var item in lst)
+                {
+                    txtOutput.Text += item + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
 
-            
+
 
             // Sustituirlos
         }
